d24056dbaab3db4dac6cf24f50851508094e3afe
readjfb <15671813+readjfb@users.noreply.github.com>
2022-05-18 18:12:53 -0500

    Updated pinmap!

--- PID_Robot/src/PIN_MAP.cpp
+++ PID_Robot/src/PIN_MAP.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
 
-#define motor1_PWM 14
-#define motor1_dir 15
-#define motor1_encoderA 11
-#define motor1_encoderB 10
+#define motorL_PWM 14
+#define motorL_dir 15
+#define motorL_encoderA 11
+#define motorL_encoderB 10
 
 #define LED_PIN 13
 
-#define motor2_PWM 5
-#define motor2_dir 4
-#define motor2_encoder_A 8
-#define motor2_encoder_B 7
+#define motorR_PWM 5
+#define motorR_dir 4
+#define motorR_encoder_A 7
+#define motorR_encoder_B 6
+
+#define buttonPin 22
